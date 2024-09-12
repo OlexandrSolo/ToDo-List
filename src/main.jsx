19f1af4f3c6376b2c614a75f454304d2abfcdb10
Your Name <you@example.com>
2024-09-12 21:27:56 +0300

    base bundle

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// 1. Імпортуємо провайдер
+import { Provider } from "react-redux";
+// 2. Імпортуємо створений раніше стор
+import { store } from "./redux/store";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
